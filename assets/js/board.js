ae34dc7348aabcd90b370f9b1ac6b376c78684c4
andreascodes <165793693+andreascodes@users.noreply.github.com>
2024-05-11 13:43:18 +0200

    Update board.js

--- assets/js/board.js
+++ assets/js/board.js
@@ -2,11 +2,20 @@ let toDo = document.getElementById('toDoContainer');
 let inProgress = document.getElementById('inProgressContainer');
 let awaitFeedback = document.getElementById('awaitFeedbackContainer');
 
+let Tasks;     // String der alle Task beinhaltet
+
 function renderBoard(){
     toDoContainer();    //load to do´s 
     inProgressContainer();       //load tasks in progress
     awaitFeedbackContainer();    //load await feedback
+    function downloadData(){} //load from server, actualise TaskString
+
 }
+
+function downloadData(){} //load from server
+    
+
+function uploadData(){} //upload to server
 
 function findTask(){
     let SearchedTask  = document.getElementById('boardInput').value
@@ -27,8 +36,11 @@ function findTask(){
 
 function searchResult(s){        //Text im Titel oder Beschreibung --> Array Tasks noch auf API umschreiben
     //Datenbankabgleich
+
+    console.log("SearchResul übergabe:", s);
+
     for(i=0; i<Tasks.length; i++){
-        if (Tasks[i].title == a && Tasks[i].type == 0){
+        if (Tasks[i].title == s && Tasks[i].type == 0){
             toDoContainer(i);    //Card einfügen
         }
         if (Tasks[i].title == s && Tasks[i].type == 1){
