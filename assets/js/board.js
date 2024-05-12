9072a98a9a008512188b0f713d76e8297d3ec491
andreascodes <165793693+andreascodes@users.noreply.github.com>
2024-05-12 15:53:15 +0200

    board.js added eventlistener to boardfield

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,29 +1,37 @@
+let Tasks;     // String der alle Task beinhaltet 
+
+const Searchfield = document.getElementById('boardInput').addEventListener('input', findTask);
+
 let toDo = document.getElementById('toDoContainer');
 let inProgress = document.getElementById('inProgressContainer');
 let awaitFeedback = document.getElementById('awaitFeedbackContainer');
 
-let Tasks;     // String der alle Task beinhaltet
-
 function renderBoard(){
+    function downloadData(){} //load from server, actualise Task - Array
     toDoContainer();    //load to do´s 
     inProgressContainer();       //load tasks in progress
     awaitFeedbackContainer();    //load await feedback
-    function downloadData(){} //load from server, actualise TaskString
 
 }
 
-function downloadData(){} //load from server
-    
+function downloadData(){} //load from server    
 
 function uploadData(){} //upload to server
 
-function findTask(){
-    let SearchedTask  = document.getElementById('boardInput').value
+function testSearch (event){
+    const Suchwert = event.target.value;
+    console.log("neuer wert im Suchfeld:", Suchwert );
+}
+
+function findTask(event){
+    console.log("findTaskStarted");
+    const SearchedTask  = event.target.value;
     if (SearchedTask == ""){
         console.log("input field empty");
+        renderBoard();                //Searchfield empty again -> reload up do date status
         return;
     }
-    console.log(SearchedTask);
+    console.log("Aktueller Suchfeldwert:",SearchedTask);
     let toDo = document.getElementById('toDoContainer');
     let inProgress = document.getElementById('inProgressContainer');
     let awaitFeedback = document.getElementById('awaitFeedbackContainer');
@@ -37,7 +45,7 @@ function findTask(){
 function searchResult(s){        //Text im Titel oder Beschreibung --> Array Tasks noch auf API umschreiben
     //Datenbankabgleich
 
-    console.log("SearchResul übergabe:", s);
+    console.log("SearchResult übergeben:", s);
 
     for(i=0; i<Tasks.length; i++){
         if (Tasks[i].title == s && Tasks[i].type == 0){
