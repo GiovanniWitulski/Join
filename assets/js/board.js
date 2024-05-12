d6c2e15c07b94e52a48d4855b0ae13cb062b35b6
andreascodes <165793693+andreascodes@users.noreply.github.com>
2024-05-12 19:13:38 +0200

    board.js JSON Array "Task" testweise befüllt für Erstellung von Cards/Tasks

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,10 +1,55 @@
-let Tasks;     // String der alle Task beinhaltet 
+
+// String der alle Task beinhalt, Testweise befüllt
+
+let Tasks = [           
+    {
+        "label": "/assets/svg/Labels Board card label technical task.svg",
+        "title": "Technical Task Example",
+        "description": "To Do Example",
+        "date": "03.05.2035",
+        "subtask" : ["check this subtask.", "click on the small box to check it."],
+        "priority" : ["medium", "/assets/svg/capa_1_medium_priority.svg"],
+        "assignedTo" : ["Max Mustermann", "Richard Roberts"], // oder Id's der Mitarbeiter
+        "type" : "0",
+        "taskid": "1"
+    },
+    {
+        "label": "/assets/svg/Labels_Board_card_label_blue_User_Story.svg",
+        "title": "User Story Example",
+        "description": "In Progress Example",
+        "date": "10.05.2035",
+        "subtask" : ["check this subtask.", "click on the small box to check it."],
+        "priority" : ["urgent", "/assets/svg/Capa_2_Burger menue_Arrow_up.svg"],
+        "assignedTo" : ["Max Mustermann", "Richard Roberts"], // oder Id's der Mitarbeiter
+        "type" : "1",
+        "taskid": "2"
+    },
+    {
+        "label": "/assets/svg/Labels_Board_card_label_blue_User_Story.svg",
+        "title": "User Story Example",
+        "description": "Await Feedback Example",
+        "date": "08.05.2035",
+        "subtask" : ["check this subtask.", "click on the small box to check it."],
+        "priority" : ["low", "/assets/svg/capa_priority_low.svg"],
+        "assignedTo" : ["Max Mustermann", "Richard Roberts"], // oder Id's der Mitarbeiter
+        "type" : "2",
+        "taskid": "3"
+    }
+];                         
+
+
+//global variables
 
 const Searchfield = document.getElementById('boardInput').addEventListener('input', findTask);
-
 let toDo = document.getElementById('toDoContainer');
 let inProgress = document.getElementById('inProgressContainer');
 let awaitFeedback = document.getElementById('awaitFeedbackContainer');
+
+//functions
+
+function downloadData(){} //load from server    
+
+function uploadData(){} //upload to server
 
 function renderBoard(){
     function downloadData(){} //load from server, actualise Task - Array
@@ -14,18 +59,9 @@ function renderBoard(){
 
 }
 
-function downloadData(){} //load from server    
-
-function uploadData(){} //upload to server
-
-function testSearch (event){
-    const Suchwert = event.target.value;
-    console.log("neuer wert im Suchfeld:", Suchwert );
-}
-
 function findTask(event){
     console.log("findTaskStarted");
-    const SearchedTask  = event.target.value;
+    const SearchedTask = event.target.value;
     if (SearchedTask == ""){
         console.log("input field empty");
         renderBoard();                //Searchfield empty again -> reload up do date status
@@ -50,25 +86,48 @@ function searchResult(s){        //Text im Titel oder Beschreibung --> Array Tas
     for(i=0; i<Tasks.length; i++){
         if (Tasks[i].title == s && Tasks[i].type == 0){
             toDoContainer(i);    //Card einfügen
-        } else if (Tasks[i].description && Tasks[i].type == 0){
-            toDoContainer(i);
+        } else if (Tasks[i].description == s && Tasks[i].type == 0){
+            toDoContainer(i);   //Card einfügen
         }
         if (Tasks[i].title == s && Tasks[i].type == 1){
-            inProgressContainer(i); `` //Card einfügen
-        } else if (Tasks[i].description && Tasks[i].type == 1){
-            inProgressContainer(i);
+            inProgressContainer(i);   //Card einfügen
+        } else if (Tasks[i].description == s && Tasks[i].type == 1){
+            inProgressContainer(i);  //Card einfügen
         }
         if (Tasks[i].title == s && Tasks[i].type == 2){
-            awaitFeedbackContainer(i);
-        } else if (Tasks[i].description && Tasks[i].type == 2){
-            awaitFeedbackContainer(i);
+            awaitFeedbackContainer(i);  //Card einfügen
+        } else if (Tasks[i].description == s && Tasks[i].type == 2){
+            awaitFeedbackContainer(i);  //Card einfügen
         }
        }
 
 }
     
 function toDoContainer (){
-    let toDo = document.getElementById('toDoContainer'); 
+
+    for(i=0; i<Tasks.length; i++){
+        const toDoCard = Task[i];
+        if (toDo.type == 0){
+            toDo.innerHTML += `
+        <div class="card-body">
+        <div id="cardHeader" class="card-header"><img src="" alt="label"></div>
+        <div id="cardTitle" class="card-title"><h1></h1></div>
+        <div id="cardDescription" class="card-description"><h3></h3></div>
+        <div id="dueDate" class="due-date"><h3></h3></div>
+        <div class="assigned-to">
+        <div id="assignedTo"><h3>Assigned to:</h3><div id="participants" class="participants"></div>
+        </div>
+        <div class="card-subtasks">
+        <div id="cardSubtasks"><h3>Subtasks</h3></div>
+        <div id="cardChecklist" class="checklist"></div>
+        </div>
+        <div class="delete-edit">
+        <div><img src="/assets/svg/delete.svg" alt="delete"><h3>Delete</h3></div><div><img src="/assets/svg/edit.svg" alt="Edit"><h3>Edit</h3></div>
+        </div>
+        </div>
+         `
+        }
+    }
 
     //add Task - Cards with .innerHTML //
     // Label abfragen --> if label1 ---> img src "/img/label1.svg...", 
