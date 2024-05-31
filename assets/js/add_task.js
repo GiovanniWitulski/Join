a17378e0b5eb45dcb3f5552bc5ad455b79169f79
Johannes Kroh <152779723+JoCro@users.noreply.github.com>
2024-05-31 18:09:55 +0200

    updated js

--- assets/js/add_task.js
+++ assets/js/add_task.js
@@ -1,6 +1,3 @@
-
-
-
 let subtaskCount = 0;
 let priorityOfTask;
 let idOfNewTask;
@@ -12,42 +9,35 @@ function changeToActive(id){
     console.log(priorityOfTask);
     let element = document.getElementById(id)
     element.classList.toggle(`${id}-active`);
-    
     for (let i = 0; i < possibleId.length; i++) {
         const element = possibleId[i];
         document.getElementById(element).classList.remove(`${element}-active`);
-        
     }
 }
 
+
 function changeButtons(){
-    
     if (document.getElementById('subtask-buttons').classList.contains('none')){
         document.getElementById('subtask-buttons').classList.remove('none');
         document.getElementById('subtask-plus-button-div').classList.add('none');
     }
-    
 }
+
 
 function onSubtaskBlur(){
     let inputfield = document.getElementById('subtask-input');
-    
     if(inputfield.value === ''){
         if(!document.getElementById('subtask-buttons').classList.contains('none')){
             document.getElementById('subtask-buttons').classList.add('none');
             document.getElementById('subtask-plus-button-div').classList.remove('none');
         }
     }
-    
-    
 }
+
 
 function addBlurListener(){
     let subtaskInput = document.getElementById('subtask-input');
-    
     if(subtaskInput){
-        
-        
         subtaskInput.addEventListener("blur", onSubtaskBlur);
     }
 }
@@ -58,20 +48,16 @@ function addToSubtasks(){
     subtaskCount += 1;
     let inputfield = document.getElementById('subtask-input');
     let subtaskList = document.getElementById('list-of-subtasks');
-    
-    
     subtaskList.innerHTML += `<li id=subtask${subtaskCount}><div class="content-of-subtask"><div id="textOfSubtask${subtaskCount}">${inputfield.value}</div> <div class="edit-subtask-div"><button class="edit-btn" onclick="editSubtask('${subtaskCount}')"></button><div class="btn-divider"></div><button onclick="deleteElementById('subtask${subtaskCount}')" class="trash-btn"></button></div></div></li>`;
     inputfield.value = ``;
     onSubtaskBlur();
-    
-    
 }
+
 
 function editSubtask(id){
     let subtaskToChange = document.getElementById(`subtask${id}`);
     let valueToInsert = document.getElementById(`textOfSubtask${id}`).innerHTML;
     if(subtaskToChange){
-        
         let inputToEdit = document.createElement('div');
         inputToEdit.setAttribute('id', `subtask${id}`);
         inputToEdit.innerHTML = `<input type="text" value="${valueToInsert}" id="edit-${subtaskToChange.getAttribute('id')}"><div class="edit-subtask-buttons"><button onclick="deleteSubtask('subtask${id}')" class="trash-btn"></button><div class="btn-divider"></div><button onclick="confirmEditedSubtask('${inputToEdit.id}', '${id}')" class="correct-btn"></button></div>`
@@ -85,12 +71,9 @@ function editSubtask(id){
 
 
 function confirmEditedSubtask(id, functionValue){
-    
     let subtask = document.getElementById(id);
     let editedText = document.getElementById(`edit-subtask${functionValue}`).value;
-    
-    if(subtask){
-        
+    if(subtask){   
         let subtaskToConfirm = document.createElement('li');
         subtaskToConfirm.setAttribute('id', id);
         subtaskToConfirm.innerHTML = `<div class="content-of-subtask"><div id="textOfSubtask${functionValue}">${editedText}</div><div class="edit-subtask-div"><button class="edit-btn" onclick="editSubtask('${functionValue}')"></button><div class="btn-divider"></div><button class="trash-btn" onclick="deleteElementById('subtask${functionValue}')"></button></div></div>`
@@ -98,21 +81,16 @@ function confirmEditedSubtask(id, functionValue){
     }else{
         console.log("error");
     }
-    
-    
-    
 }
 
 
 function deleteSubtask(id){
     let subtaskToDelete = document.getElementById(id);
-    subtaskToDelete.parentNode.removeChild(subtaskToDelete);
-    
+    subtaskToDelete.parentNode.removeChild(subtaskToDelete);  
 }
 
 
 function cancelSubtask(){
-    
     let inputfield = document.getElementById('subtask-input');
     inputfield.value = '';
     onSubtaskBlur();
@@ -132,16 +110,12 @@ function cancelSubtask(){
 }*/
 
 function deleteElementById(elementId){
-    
     let element = document.getElementById(elementId);
     if (element){
-        element.parentNode.removeChild(element);
-        
-        
+        element.parentNode.removeChild(element);   
     }else{
         console.log("The Element with the inserted ID can´t be found");
     }
-    
 }
 
 
@@ -158,7 +132,6 @@ function renderAssignSelector(){
             </svg><div class="contactNames">${contact['vorname']} ${contact['name']}</div></div><img id="checkbox${i}" class="checkbox-icon" src="/assets/svg/rectangle.svg"></div>`;
         }
     }
-    
 }
 
 
@@ -174,18 +147,16 @@ function assignTheContact(element, checkboxID){
     assignPicturesToDiv();
 }
 
+
 function assignPicturesToDiv(){
-    
     let checkedContacts = document.getElementsByClassName('checked');
     document.getElementById('assignedContactPics').innerHTML = ``;
-    
     for (let index = 0; index < checkedContacts.length; index++) {
         const element = checkedContacts[index];
         document.getElementById('assignedContactPics').innerHTML += `<div>${element['childNodes'][0]['firstChild'].outerHTML}</div>`;
-        
     }
-    
 }
+
 
 function showContactsToAssign(){
     document.getElementById('optionContainer').classList.remove('none');
@@ -196,45 +167,45 @@ function showContactsToAssign(){
 
 
 function filterAssignedContacts(){
-    
     let assignableContacts = document.getElementsByClassName('contactNames');
     let input = document.getElementById('contact-selector');
     let filter = input.value.toLowerCase();
-    
     for (let i = 0; i < assignableContacts.length; i++) {
         const element = assignableContacts[i];
         const text = element.innerText;
         if(!text.toLowerCase().includes(filter)){
-            
-            document.getElementById(`contact${i}`).classList.add('none');
-            document.getElementById(`contact${i}`).classList.remove('option');
-            
+            hideThisContact(`contact${i}`);
         }else{
-            document.getElementById(`contact${i}`).classList.add('option');
-            document.getElementById(`contact${i}`).classList.remove('none');
+            showThisContact(`contact${i}`);
             if(!document.getElementById(`contact${i}`).classList.contains('checked')){
                 document.getElementById(`checkbox${i}`).src = '/assets/svg/rectangle.svg';
-            }
-            
-            
-            
+            }   
         }
-        
     }
 }
 
+
+function hideThisContact(id){
+    document.getElementById(id).classList.add('none');
+    document.getElementById(id).classList.remove('option');
+}
+
+
+function showThisContact(id){
+    document.getElementById(id).classList.add('option');
+    document.getElementById(id).classList.remove('none');
+}
+
+
 function addEventListenerToInput(){
-    
     document.getElementById('contact-selector').addEventListener('input',filterAssignedContacts);
 }
 
 function hideContactsToAssign(button){
-    
     document.getElementById('optionContainer').classList.add('none');
     if(button.classList.contains('active')){
         button.classList.toggle('active');
-    }
-    
+    } 
 }
 
 
@@ -252,33 +223,25 @@ async function getTheDataForPostTask() {
         type: "0",
         taskid: await getTheIdOfTask()
     };
-  await postData('tasks', data);
-  document.getElementById('addTaskForm').submit();
+    await postData('tasks', data);
+    document.getElementById('addTaskForm').submit();
 }
 
 
 async function getTheIdOfTask(){
-
     let id = await loadData('taskId');
-    
     if(id != null){
-
         idOfNewTask = id + 1;
-       await putData('taskId', idOfNewTask);
-       console.log(idOfNewTask);
-
+        await putData('taskId', idOfNewTask);
     }else{
         idOfNewTask = 0;
-       await putData('taskId', idOfNewTask);
-       console.log(idOfNewTask);
+        await putData('taskId', idOfNewTask);
     }
-
-    return idOfNewTask;
-
+    return idOfNewTask;  
 }
 
+
 function getTheSubtaskStatus(subtasks){
-    
     let subtaskStatus = [];
     for (let index = 0; index < subtasks.length; index++) {
         const element = subtasks[index];
@@ -289,47 +252,40 @@ function getTheSubtaskStatus(subtasks){
 
 
 function getTheCategoryOfTask(){
-    
     let selection = document.getElementsByClassName('selector dropdown-img');
     let selectedCategory = selection[0].options.selectedIndex;
     return selectedCategory
-    
 }
 
+
 function getTheSubtasksOfTask(){
-    
     let listOfSubtasks = document.getElementsByClassName('content-of-subtask');
     let subtasksAsText = [];
     for (let index = 0; index < listOfSubtasks.length; index++) {
         const subtask = listOfSubtasks[index].childNodes[0].innerHTML;
         subtasksAsText.push(subtask);
-        
     }
-    
     return subtasksAsText;
-    
 }
 
 
 function getTheEmblemsOfAssignedContacts(){
-    
     let listOfCheckedContacts = document.getElementsByClassName('checked');
     let listOfContactEmblems = [];
     for (let index = 0; index < listOfCheckedContacts.length; index++) {
         const emblem = listOfCheckedContacts[index].childNodes[0].firstChild.outerHTML;
         listOfContactEmblems.push(emblem);
-        
     }
     return listOfContactEmblems;
 }
+
 
 function fetchAllAssignedContacts(){
     let listOfCheckedContacts = document.getElementsByClassName('checked');
     let listOfAssignedNames = [];
     for (let i = 0; i < listOfCheckedContacts.length; i++) {
         const contact = listOfCheckedContacts[i].childNodes[0].children[1].innerHTML;
-        listOfAssignedNames.push(contact)
-        
+        listOfAssignedNames.push(contact);
     }
     return listOfAssignedNames;
 }
