64e9a4cf7470f50f77e022dcb1c90d221d2c89a6
andreascodes <165793693+andreascodes@users.noreply.github.com>
2024-06-23 17:41:54 +0200

    +update2

--- assets/js/board.js
+++ assets/js/board.js
@@ -152,11 +152,13 @@ function allowDrop(ev) {
   }
 
 function dropAt(newType){
+    console.log ("dataUploadDrop");
     for (i=0; i<TaskBoard.length; i++){
         taskToMove = TaskBoard[i].taskid;
         if (taskToMove == currentDraggedTask){
             TaskBoard[i].type = newType;
-            renderBoard();
+            uploadData();
+            renderBoard();            
             return;
         }
     }
@@ -546,100 +548,113 @@ function doneContainer(){
 // OVERLAY TASK / POPUP ///////////////////////
 
 
-function OverlayTaskPopup(i){   // TO CODE/CONNECT: EditTaskFkt  
-        document.getElementById('mobileTamplateContent').classList.add('background-fade');
-        document.getElementById('Board').classList.add('background-fade');
+function OverlayTaskPopup(i) {
+    document.getElementById('mobileTamplateContent').classList.add('background-fade');
+    document.getElementById('Board').classList.add('background-fade');
+    const OverlayTask = TaskBoard[i];
+    let emblem = '';
+    let overlayContacts = '';
+    for (let c = 0; c < OverlayTask.contactEmblem.length; c++) {
+        const contactName = OverlayTask.assignedTo[c];
+        const src = OverlayTask.contactEmblem[c];
+        emblem = src;
+        overlayContacts += '<div class="overlay-assigned-to-contacts">' + emblem + '<div class="overlay-contact-name">' + contactName + '</div></div>';
+    }
 
-        const OverlayTask = TaskBoard[i];
+    let priority = '';
+    if (OverlayTask.priority === "low") {
+        priority = "/assets/svg/capa_priority_low.svg";
+    } else if (OverlayTask.priority === "medium") {
+        priority = "/assets/svg/capa_1_medium_priority.svg";
+    } else if (OverlayTask.priority === "urgent") {
+        priority = "/assets/svg/Capa_2_Burger menue_Arrow_up.svg"
+    }
 
-        let emblem = '';
-        let overlayContacts = '';
-         for (let c = 0; c < OverlayTask.contactEmblem.length; c++){
-            const contactName = OverlayTask.assignedTo[c];
-            const src = OverlayTask.contactEmblem[c];
-            emblem = src;            
-            overlayContacts += '<div class="overlay-assigned-to-contacts">'+emblem+'<div class="overlay-contact-name">'+contactName+'</div></div>';
-        }         
-        
-        let priority = '';
-        if (OverlayTask.priority === "low"){
-            priority = "/assets/svg/capa_priority_low.svg";
-        }   else if (OverlayTask.priority === "medium"){
-            priority = "/assets/svg/capa_1_medium_priority.svg";
-        }   else if(OverlayTask.priority === "urgent") {
-            priority = "/assets/svg/Capa_2_Burger menue_Arrow_up.svg"}
+    const checkmark0 = OverlayTask.subtaskSum[0];
+    const checkmark1 = OverlayTask.subtaskSum[1];
+    let check0 = '';
+    let check1 = '';
+    let check = "/assets/svg/checkmark.svg";
+    let nocheck = "/assets/svg/rectangle.svg";
+    if (checkmark0 == 0) { check0 = nocheck; } else { check0 = check; }
+    if (checkmark1 == 0) { check1 = nocheck; } else { check1 = check; }
 
+    let label;
+    if (OverlayTask.label == 1) {
+        label = TechnicalTaskLabel;
+    } else {
+        label = UserStoryLabel;
+    }
 
-        const checkmark0 = OverlayTask.subtaskSum[0];
-        const checkmark1 = OverlayTask.subtaskSum[1];
-        let check0 = '';   let check1 = ''; 
-        let check = "/assets/svg/checkmark.svg"; let nocheck = "/assets/svg/rectangle.svg";
-        if (checkmark0 == 0){check0 = nocheck;} else {check0 = check;}
-        if (checkmark1 == 0){check1 = nocheck;} else {check1 = check;}
-        
-        let label;
-        if(OverlayTask.label == 1){
-            label = TechnicalTaskLabel;
-        } else {
-            label = UserStoryLabel;
-        }            
-
-        Overlay.innerHTML += `  
-        <div id="${OverlayTask.taskid}" class="overlay-container">            
-        <div class="overlay-task">
-        <div id="overlayHeader" class="overlay-card-header">${label}<img onclick="closeOverlay()" src="/assets/svg/close_black.svg" alt="close"></div>
-        <div id="overlayTitle" class="overlay-card-title">${OverlayTask.title}</div>
-        <div id="overlayDescription" class="overlay-card-description">${OverlayTask.description}</div>
-        <div id="overlaydueDate" class="overlay-card-due-date"><div id="overlayDueDate">Due date:</div><div class="overlay-due-date">${OverlayTask.date}</div></div>
-        <div id="overlaypriority" class="overlay-card-priority"><div id="overlayPriorityText" class="overlay-card-priority-text">
-        Priority</div><div class="overlay-card-priority-text-img"><img src="${priority}" alt="priority">${OverlayTask.priority}</div>
+    Overlay.innerHTML = `
+        <div id="${OverlayTask.taskid}" class="overlay-container">
+            <div class="overlay-task">
+                <div id="overlayHeader" class="overlay-card-header">${label}<img onclick="closeOverlay()" src="/assets/svg/close_black.svg" alt="close"></div>
+                <div id="overlayTitle" class="overlay-card-title">${OverlayTask.title}</div>
+                <div id="overlayDescription" class="overlay-card-description">${OverlayTask.description}</div>
+                <div id="overlaydueDate" class="overlay-card-due-date"><div id="overlayDueDate">Due date:</div><div class="overlay-due-date">${OverlayTask.date}</div></div>
+                <div id="overlaypriority" class="overlay-card-priority"><div id="overlayPriorityText" class="overlay-card-priority-text">
+                Priority</div><div class="overlay-card-priority-text-img"><img src="${priority}" alt="priority">${OverlayTask.priority}</div>
+                </div>
+                <div id="overlayAssignedTo" class="overlay-assigned-to">
+                <div id="overlayAssignedToText" class="overlay-assigned-to-text">Assigned to:</div><div id="overlayParticipants" class="overlay-participants">${overlayContacts}</div>
+                </div>
+                <div class="overlay-card-subtasks">
+                <div id="overlaySubstasksText" class="overlay-substasks-text">Subtasks:</div><div class="overlay-checkbox"><div id="overlaySubtask0" class="overlay-subtask"><img class="overlay-checkbox-img" onclick="toggleCheckboxValue(${OverlayTask.taskid}, 0, ${i})"  src=${check0}> ${OverlayTask.subtask[0]}</div>
+                <div id="overlaySubtask1" class="overlay-subtask"><img class="overlay-checkbox-img" onclick="toggleCheckboxValue(${OverlayTask.taskid}, 1, ${i})"  src=${check1}> ${OverlayTask.subtask[1]}</div></div>
+                </div>
+                <div class="overlay-card-delete-edit">
+                <div onclick="overlayDeleteTask(${OverlayTask.taskid}, ${i})" class="overlay-cde"><img class="overlay-cde-img1" src="/assets/svg/delete.svg" alt="delete">Delete</div><div class="placeholder-div">|</div><div onclick="editTaskOverlay(${OverlayTask.taskid}, ${i})" class="overlay-cde"><img class="overlay-cde-img2" src="/assets/svg/edit.svg" alt="Edit">Edit</div>
+                </div>
+            </div>
         </div>
-        <div id="overlayAssignedTo" class="overlay-assigned-to">
-        <div id="overlayAssignedToText" class="overlay-assigned-to-text">Assigned to:</div><div id="overlayParticipants" class="overlay-participants">${overlayContacts}</div>  
-        </div>
-        <div class="overlay-card-subtasks">
-        <div id="overlaySubstasksText" class="overlay-substasks-text">Subtasks:</div><div class="overlay-checkbox"><div id="overlaySubtask0" class="overlay-subtask"><img class="overlay-checkbox-img" onclick="toggleCheckboxValue(${OverlayTask.taskid}, 0, ${i})"  src=${check0}> ${OverlayTask.subtask[0]}</div>
-        <div id="overlaySubtask1" class="overlay-subtask"><img class="overlay-checkbox-img" onclick="toggleCheckboxValue(${OverlayTask.taskid}, 1, ${i})"  src=${check1}> ${OverlayTask.subtask[1]}</div></div>
-        </div>
-        <div class="overlay-card-delete-edit">
-        <div onclick="overlayDeleteTask(${OverlayTask.taskid}, ${i})" class="overlay-cde"><img class="overlay-cde-img1" src="/assets/svg/delete.svg" alt="delete">Delete</div><div class="placeholder-div">|</div><div onclick="editTaskOverlay(${OverlayTask.taskid}, ${i})" class="overlay-cde"><img class="overlay-cde-img2" src="/assets/svg/edit.svg" alt="Edit">Edit</div>
-        </div>
-        </div>
-        </div>
+    `;
 
-        `
+    if (OverlayTask.subtask[0] === undefined) {
+        document.getElementById('overlaySubtask0').style.display = 'none';
+    }
+    if (OverlayTask.subtask[1] === undefined || OverlayTask.subtask[1] === null) {
+        console.log("subtask1=0");
+        document.getElementById('overlaySubtask1').style.display = 'none';
+        document.getElementById('overlaySubtask1').classList.add('hide');
+    }
 
-        if (OverlayTask.subtask[0] === undefined){
-            document.getElementById('overlaySubtask0').style.display = 'none';
-        }
-        if (OverlayTask.subtask[1] === undefined){
-            document.getElementById('overlaySubtask1').style.display = 'none';
-        }
-
-        if (OverlayTask.label === 1){
-            document.getElementById('overlayDueDate').style.cssText = 'font-weight: 700; color: #42526E;';
-            document.getElementById('overlayAssignedToText').style.cssText = 'font-weight: 700; color: #42526E;';
-            document.getElementById('overlayPriorityText').style.cssText = 'font-weight: 700; color: #42526E;';
-            document.getElementById('overlaySubstasksText').style.cssText = 'font-weight: 700; color: #42526E;';
-        }   
-} 
+    if (OverlayTask.label === 1) {
+        document.getElementById('overlayDueDate').style.cssText = 'font-weight: 700; color: #42526E;';
+        document.getElementById('overlayAssignedToText').style.cssText = 'font-weight: 700; color: #42526E;';
+        document.getElementById('overlayPriorityText').style.cssText = 'font-weight: 700; color: #42526E;';
+        document.getElementById('overlaySubstasksText').style.cssText = 'font-weight: 700; color: #42526E;';
+    }
+}
 
 function toggleCheckboxValue(taskid, position, o) {
     console.log("toggleCheckboxValue id: position:", taskid, position);
-    for (i=0; i<TaskBoard.length; i++){
+    for (let i = 0; i < TaskBoard.length; i++) {
         const findTask = TaskBoard[i].taskid;
         let subtaskValue = TaskBoard[i].subtaskSum[position];
-        if (findTask == taskid){   
-            console.log("taskfound at arrayposition:", i);
-            console.log(" alter sumbSumvalue:", subtaskValue);            
+        if (findTask == taskid) {
+            console.log("task found at array position:", i);
+            console.log("old subtaskSum value:", subtaskValue);
             if (subtaskValue === 0) {
                 TaskBoard[i].subtaskSum[position] = 1;
             } else {
                 TaskBoard[i].subtaskSum[position] = 0;
-             }            
-             console.log("Neuer sumbSumvalue:", TaskBoard[i].subtaskSum[position]);
-        }            
+            }
+            console.log("new subtaskSum value:", TaskBoard[i].subtaskSum[position]);
+        }
     }
+
+    // Aktualisiere die Anzeigeeigenschaften der Subtasks vor dem Aufruf von OverlayTaskPopup
+    const OverlayTask = TaskBoard[o];
+    if (OverlayTask.subtask[0] === undefined) {
+        document.getElementById('overlaySubtask0').style.display = 'none';
+    }
+    if (OverlayTask.subtask[1] === undefined || OverlayTask.subtask[1] === null) {
+        console.log("subtask1=0");
+        document.getElementById('overlaySubtask1').style.display = 'none';
+        document.getElementById('overlaySubtask1').classList.add('hide');
+    }
+
     OverlayTaskPopup(o);
     renderBoard();
 }
