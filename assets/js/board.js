49e7f8c83f12b54489551ec6f8e00bf69ee32cf0
andreascodes <165793693+andreascodes@users.noreply.github.com>
2024-07-02 18:38:16 +0200

    save

--- assets/js/board.js
+++ assets/js/board.js
@@ -214,7 +214,7 @@ async function overlayRender(){
     <div id="overlaySubtaskContainer">${overlaySubtaskStorage}</div>
     </div>        
     <div class="overlay-card-delete-edit">
-    <div onclick="overlayDeleteTask(${OverlayTask.taskid}, ${OverlayTaskBoardPosition})" class="overlay-cde"><img class="overlay-cde-img1" src="/assets/svg/delete.svg" alt="delete">Delete</div><div class="placeholder-div">|</div><div onclick="editTaskOverlay(${OverlayTask.taskid}, ${OverlayTaskBoardPosition})" class="overlay-cde"><img class="overlay-cde-img2" src="/assets/svg/Subtasks%20icons11-4.svg" alt="Edit">Edit</div>
+    <div onclick="overlayDeleteTask(${OverlayTask.taskid}, ${OverlayTaskBoardPosition})" class="overlay-cde"><div class="overlay-cde-delete-svg"></div>Delete</div><div class="placeholder-div">|</div><div onclick="editTaskOverlay(${OverlayTask.taskid}, ${OverlayTaskBoardPosition})" class="overlay-cde-edit"><div class="overlay-cde-edit-svg"></div>Edit</div>
     </div>
     </div>
     </div>
