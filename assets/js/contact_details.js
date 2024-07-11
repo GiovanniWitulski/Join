c2c2e93fa01529eccbad924012f031d070d27798
Johannes Kroh <152779723+JoCro@users.noreply.github.com>
2024-07-11 15:49:25 +0200

    completed js-doc

--- assets/js/contact_details.js
+++ assets/js/contact_details.js
@@ -20,6 +20,10 @@ if (moreButton && moreBtnMenu) {
     document.addEventListener('click', closeMenuOnClickOutside);
 } 
 
+/**
+ * This function is used to load the ID of the clicked contact. The function determines whether the user is on the desktop or mobile version and executes the respective function of the version
+ * 
+ */
 async function loadCurrentContactId(){
     let userId = await loadData('currentContact');
     if(window.innerWidth >= 1250){
@@ -29,6 +33,11 @@ async function loadCurrentContactId(){
     }
 }
 
+/**
+ * This function is used to render the contact-information of the contact the user wants to display. It fetches the data of the contact from the contact-array contactsAsJson <Mobile Version>
+ * 
+ * @param {string} id - the id of the current contact, which is stored in the firebase database. It´s passed by the function loadCurrentContactId 
+ */
 async function loadSingleContact(id){
     let user;
     for (let i = 0; i < contactsAsJson.length; i++) {
@@ -45,6 +54,11 @@ async function loadSingleContact(id){
     document.getElementsByClassName('edit-btn')[0].outerHTML = `<button class="edit-btn" onclick="fillEditContactForm(${id})"></button>`
 }
 
+/**
+ * This function is used to render the contact-information of the contact the user wants to display. It fetches the data of the contact from the contact-array contactsAsJson <Desktop Version>
+ * 
+ * @param {string} id - the id of the current contact, which is stored in the firebase database. It´s passed by the function loadCurrentContactId 
+ */
 async function loadSingleContactDesktop(id){
     let user;
     for (let i = 0; i < contactsAsJson.length; i++) {
@@ -61,27 +75,55 @@ async function loadSingleContactDesktop(id){
     document.getElementsByClassName('edit-btn')[0].outerHTML = `<button class="edit-btn" onclick="fillEditContactFormDesktop(${id})"></button>`
 }
 
+/**
+ * This function is used to determine the token of the contact the user wants to delete. Then the contact is deleted from the firebase database 
+ * 
+ * @param {string} id - the id of the contact to delete. Given by the function loadSingleContact. It represents the index of the contact in the contactsAsJson-array 
+ */
 async function deleteContact(id){
     tokenToDelete = contactsAsJson[id]['id'];
     await deleteData(`contacts/${tokenToDelete}`);
     window.location.href = "/contacts.html";
 }
 
+/**
+ * This function is used to create a profile pic for the contact the user wants to display in its required size 
+ * 
+ * @param {Array} user - this is the position of the contact to load in the contactsAsJson-Array ... f.e. contactsAsJson[2]
+ * @returns {string} - HTML Code as a String
+ */
 function drawContactDetailPic(user){
     return `<svg class="name-pic" width="42px" height="42px"><circle cx="40" cy="40" r="40" stroke="white" stroke-width="2" fill="${user['color']}" />
     <text x="20" y="48" fill="white" font-size="27px">${user['vorname'].charAt(0)}${user['name'].charAt(0)}</text></svg>`;
 }
 
+/**
+ * This function is used to create profile pic for the contact the user wants to edit in its required size for the mobile-version
+ * 
+ * @param {Array} user - this is the position of the contact to load in the contactsAsJson-Array ... f.e. contactsAsJson[2] 
+ * @returns {string} - HTML Code as a String
+ */
 async function drawContactEditPic(user){
     return `<svg class="contact-pic-edit" width="62px" height="62px"><circle class="circle" cx="60" cy="60" r="58" stroke="white" stroke-width="2" fill="${user['color']}" />
     <text class="circle-text" x="50%" y="55%" fill="white" font-size="47px">${user['vorname'].charAt(0)}${user['name'].charAt(0)}</text></svg>`;
 }
 
+/**
+ * This function is used to create a profile pic for the contact the user wants to edit in its required size for the desktop-version
+ * 
+ * @param {*} user - this is the position of the contact to load in the contactsAsJson-Array ... f.e. contactsAsJson[2]
+ * @returns {string} - HTML Code as a String
+ */
 async function drawContactEditPicDesktop(user){
     return `<svg id="editContactPic-desktop" class="contact-pic" width="62px" height="62px"><circle class="circle" cx="60" cy="60" r="58" stroke="white" stroke-width="2" fill="${user['color']}" />
     <text class="circle-text" x="20%" y="60%" fill="white" font-size="47px">${user['vorname'].charAt(0)}${user['name'].charAt(0)}</text></svg>`;
 }
 
+/**
+ * This function is used to load the contacts data in the inputfields of the Edit-formular (mobile), which shows up when the user clicks the edit-button
+ * 
+ * @param {string} id - the index of the contact to edit in the array contactsAsJson 
+ */
 async function fillEditContactForm(id){
     idToFind = id;
     currentContact = contactsAsJson[idToFind];
@@ -98,6 +140,11 @@ async function fillEditContactForm(id){
     document.getElementById('editContactPic').outerHTML = await drawContactEditPic(currentContact);
 }
 
+/**
+ * This function is used to load the contact's data in the inputfields of the Edit-formular (desktop), which shows up when the user clicks the edit-button
+ * 
+ * @param {string} id - the index of the contact to edit in the array contactsAsJson 
+ */
 async function fillEditContactFormDesktop(id){
     idToFind = id;
     currentContact = contactsAsJson[idToFind];
@@ -114,11 +161,18 @@ async function fillEditContactFormDesktop(id){
     document.getElementById('editContactPic-desktop').outerHTML = await drawContactEditPicDesktop(currentContact);
 }
 
+/**
+ * This function is used to display the overlay-element in which the editOverlay is displayed. It decreases the brightness of the background
+ * 
+ */
 function showEditOverlay(){
     document.getElementById('overlayVeil').classList.remove('displayNone');
     document.getElementById('overlay-editContact').classList.add('showEditContact');
 }
 
+/**
+ * This function is used to hide the overlay-element in which the editOverlay is displayed. 
+ */
 function hideEditOverlay(){
     document.getElementById('overlayVeil').classList.add('displayNone');
     document.getElementById('overlay-editContact').classList.remove('showEditContact');
@@ -134,6 +188,12 @@ function hideEditOverlayDesktop(){
     document.getElementById('overlayVeilAddContact').classList.add('none');
 }
 
+/**
+ * This function is used to fetch the value of the element, which is represented by the function-parameter. It also returns the Value. It´s executed by the saveEditsToContact-function
+ * 
+ * @param {string} element - The ID of an HTML-element
+ * @returns {string} value - The value of the HTML-element
+ */
 async function getTheEditedData(element){
     if(window.innerWidth >= 1250){
         return document.getElementById(`${element}-desktop`).value;
@@ -142,6 +202,11 @@ async function getTheEditedData(element){
     }
 }
 
+/**
+ * This function is used to fetch all the edited data of the current contact and stores them in variables. Then the function puts the new information to the firebase
+ * 
+ * @param {string} token - the token of the contact to edit, which is used to address the correct contact in the firebase database. In the contactsAsJson-Array it's represented by the id-key 
+ */
 async function saveEditsToContact(token){
     let editedName = await getTheEditedData('contact-name');
     let editedMail = await getTheEditedData('contact-mail');
@@ -157,6 +222,12 @@ async function saveEditsToContact(token){
         await submitForm(indexOfCurrentContact);
     }
     
+/**
+ * This function is used to create the edited name of the current contact
+ * 
+ * @param {string} surname - a string which is represented by the variable surname (passed by the saveEditsToContact-function)
+ * @param {string} lastName - a string which is represented by the variable lastName (passed by the saveEditsToContact-function)
+ */
 async function getTheNewName(surname, lastName){
     if(surname && lastName){
         newSurname = surname.charAt(0).toUpperCase() + surname.slice(1);
@@ -167,6 +238,16 @@ async function getTheNewName(surname, lastName){
     }
 }
 
+/**
+ * this function is used to create and return the variable data, which contains every final information about the edited contact, in the JSON-format
+ * 
+ * @param {string} editedMail - a variable, passed by the saveEditsToContact-function
+ * @param {string} editedPhone - a variable, passed by the saveEditsToContact-function
+ * @param {string} newLastName - a variable, passed by the saveEditsToContact-function
+ * @param {string} newSurname - a variable, passed by the saveEditsToContact-function
+ * @param {string} contactToUpdate - a variable, passed by the saveEditsToContact-function
+ * @returns {JSON} data - the JSON-Object, which contains every information about the edited contact
+ */
 async function getTheReadyData(editedMail,editedPhone,newLastName,newSurname,contactToUpdate){
    let data = { 
         "mail": editedMail,
@@ -178,7 +259,10 @@ async function getTheReadyData(editedMail,editedPhone,newLastName,newSurname,con
         return data;
 }
 
-    
+/**
+ * This function is used to either submit the formular (mobile-version) or hide the edit-formular and refresh all the contacts 
+ * 
+ */    
 async function submitForm(){
     if(window.innerWidth < 1250){
         document.getElementById('edit-contact-form').submit();
@@ -189,7 +273,10 @@ async function submitForm(){
     }   
 }
 
-
+/**
+ * This function adds and removes css-classes from the elements of the edit-contact-process. In the end, the edit formular and all its co-elemnts won´t be displayed anymore
+ * 
+ */
 async function blendItOut(){
     document.getElementById('edit-contact-container').classList.add('fade');
     document.getElementById('overlayVeilAddContact').classList.add('none');
@@ -197,7 +284,10 @@ async function blendItOut(){
     document.getElementById('edit-contact-container').classList.remove('fade');
 }
 
-
+/**
+ * This function gets the value of the variable boolean contactWasCreated from the local Storage and executes the showTheNotification-function, if it´s true
+ * 
+ */
 async function checkIfContactWasCreated(){
     let wasContactCreated = localStorage.getItem('contactWasCreated');
     if(wasContactCreated == "true"){
@@ -205,7 +295,10 @@ async function checkIfContactWasCreated(){
     }
 }
 
-
+/**
+ * This function displays the contact-added-notification and hides it again after a short delay. The function also clears the local storage.
+ * 
+ */
 async function showTheNotificaiton(){
     document.getElementById('notificationAddContact').classList.add('showContactAdded');
     setTimeout(() => {
@@ -213,5 +306,8 @@ async function showTheNotificaiton(){
     }, 1000);
     localStorage.clear();
 }
-    
+
+/**
+ * This eventlistener executes the checkIfContactWasCreated-function everytime the complete DOMContent has loaded
+ */
 document.addEventListener('DOMContentLoaded', checkIfContactWasCreated);
--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -4,18 +4,30 @@ let contactToDisplay = 0;
 let colors = ['#FF7A00', '#FF5EB3', '#6E52FF','#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E','#FC71FF','#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B'];
 let contactsWithoutToken = [];
 
+/**
+ * This function is used to get a random color from the Color array and returns it.
+ * 
+ * @returns {array} - returns the array and the respective index of the element... f.e colors[2]
+ */
 async function getAColor(){
     let randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
 
-
+/**
+ * This function is used to create the variable contactsAsJson which stores every contact from the firebase database. It also executes the createDivs-function
+ * 
+ */
 async function loadContacts(){
     contactsAsJson = await getContacts();
     createDivs();
 }
 
 
+/**
+ * This function reads every contact from the contactsAsJson-array, then pushes every contact without its token to the contactsWithoutToken-array
+ * 
+ */
 async function readTheTokens(){
     for(const key in contactsAsJson){
         if(contactsAsJson.hasOwnProperty(key)){
@@ -30,6 +42,10 @@ async function readTheTokens(){
 }
 
 
+/** 
+ * The function checks each letter of the alphabet to see if the first letter of a first name in the contactsAsJson array matches the current letter. If a match is found, it creates a separate container for this letter, containing all contacts whose first names start with the current letter.
+ * 
+ */
 function createDivs(){
     if(document.getElementById('contact-div')){
         document.getElementById('contact-div').innerHTML = ``;
@@ -50,6 +66,13 @@ function createDivs(){
 }
 
 
+/**
+ * This function is used to create and return HTML-Code for the current contact.
+ * 
+ * @param {array} contact - this contains the array and its index for the current contact 
+ * @param {number} j - this is the index of the contact in the array 
+ * @returns {String} - HTML Code as a String
+ */
 function generateHTMLcodeForContacts(contact, j){
     return `<a  onclick="refreshContactToLoad(${j}, 'currentContact'), changeToClicked('singleContact${j}')"  id="singleContact${j}" class="singleContact"><svg class="profile_pic" width="42px" height="42px">
     <circle cx="21" cy="21" r="20" stroke="white" stroke-width="2" fill="${contact['color']}" />
@@ -59,6 +82,11 @@ function generateHTMLcodeForContacts(contact, j){
 }
 
 
+/**
+ * This function adds the clicked-class to the current contact and removes the same class from all the other contacts
+ * 
+ * @param {string} element - the id of the HTML-element 
+ */
 function changeToClicked(element){
 let singleContacts = document.getElementsByClassName('singleContact');
 for (let index = 0; index < singleContacts.length; index++) {
@@ -69,6 +97,12 @@ document.getElementById(element).classList.add('clicked');
 }
 
 
+/**
+ * This function is used to determine which contacts details should be loaded, and then executes the respective function either for mobile or for desktop
+ * 
+ * @param {number} id - the index of the current contact as an integer 
+ * @param {string} path - the path of the firebase database in which the currentContact should be stored 
+ */
 async function refreshContactToLoad(id, path) {
     contactToDisplay = id;
     await putData(path,id)
@@ -85,6 +119,10 @@ async function refreshContactToLoad(id, path) {
 }
 
 
+/**
+ * This function is used to hide the contact's information and executes the addTranlate-function
+ * 
+ */
 async function hideContactDetails(){
     let contactContainer = document.getElementById('middle-container-desktop');
     contactContainer.classList.add('none');
@@ -93,6 +131,10 @@ async function hideContactDetails(){
 }
 
 
+/**
+ * This function is used to displays the contact's information, which is still translated. Then it executes the removeTranslate-function
+ * 
+ */
 async function showContactDetails(){
     let contactContainer = document.getElementById('middle-container-desktop');
     contactContainer.classList.remove('none');
@@ -100,11 +142,21 @@ async function showContactDetails(){
 }
 
 
+/**
+ * This function is used to remove the show-class from the element, which's result is, that the element gets translateX so it can't be seen on the screen
+ * 
+ * @param {string} element - the ID of the HTML-element 
+ */
 async function addTranslate(element){
     element.classList.remove('show');
 }
 
 
+/**
+ * This function is used to add the show-class to the element with a delay, which's result is, that the element gets translateX(0) so it can be seen on the screen again
+ * 
+ * @param {string} element - the ID of the HTML-element 
+ */
 async function removeTranslate(element){
     setTimeout(() => {
         element.classList.add('show');
@@ -112,6 +164,10 @@ async function removeTranslate(element){
 }
 
 
+/**
+ * This function is used to show the user the formular which can be used to add a new contact
+ * 
+ */
 function showAddContact(){
     document.getElementById('overlay-container').classList.add('show');
     document.getElementById('overlayVeilAddContact').classList.remove('none');
@@ -121,6 +177,10 @@ function showAddContact(){
 document.addEventListener('DOMContentLoaded', checkIfLoadContactIsNeeded);
 
 
+/**
+ * This function is used to determine, if the current window-location includes either contact or board. If one of these conditions is true, the loadContacts-function is executed
+ * 
+ */
 function checkIfLoadContactIsNeeded(){
     if(window.location.href.includes('contact') || window.location.href.includes('board')){
         loadContacts('contacts');
@@ -128,6 +188,11 @@ function checkIfLoadContactIsNeeded(){
 }
 
 
+/**
+ * This function is used to reset the values of the inputfields below and makes the element, which is represented by the function-parameter, invisible for the user
+ * 
+ * @param {string} elementToHide - the id of the HTML-element 
+ */
 function hideTheFormular(elementToHide){
     document.getElementById('contact-name').value = ``;
     document.getElementById('contact-mail').value = ``;
@@ -137,6 +202,11 @@ function hideTheFormular(elementToHide){
 }
 
 
+/**
+ * This function is used to fill the inputfields of the editContact-formular's desktop-version with the correct data and shows the formular afterwards
+ * 
+ * @param {number} id - it represents the index of the current contact, which is about to be edited by the user 
+ */
 async function fillEditContactFormDesktop(id){
     idToFind = id;
     currentContact = contactsAsJson[idToFind];
@@ -151,6 +221,10 @@ async function fillEditContactFormDesktop(id){
 }
 
 
+/**
+ * This function is used to determine which version of join the user is up to and adjusts the text of the addContact-button
+ * 
+ */
 function rewriteButton(){
     if(document.getElementsByClassName('add-contact-btn')[0]){
         if(window.innerWidth >= 1250){
